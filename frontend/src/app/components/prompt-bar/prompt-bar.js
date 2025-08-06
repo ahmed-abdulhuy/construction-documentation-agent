3c3dbe6295944d8ce40199fc59a4fbe97b3a180d
Ahmed Abdelhay <ahmelhuy@gmail.com>
2025-08-06 23:22:11 +0300

    Clean out component and add styling

--- frontend/src/app/components/prompt-bar/prompt-bar.js
+++ frontend/src/app/components/prompt-bar/prompt-bar.js
@@ -1,29 +1,41 @@
 import { useState } from "react";
 
 
+/**
+ * Submit a prompt, fetch the response from the server, and set the response state.
+ * @param {(response: string) => void} setResponse - Function to set the response state
+ * @returns {Promise<void>}
+ */
 export default function PromptBar(setResponse) {
     const [prompt, setPrompt] = useState('');
+
+    // fetch the response from the server and set the response state
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
         const res = await fetch(`http://localhost:8000/?query=${encodeURIComponent(prompt)}`);
         const data = await res.json();
-        console.log(data);
-        console.log('\n');
-        console.log(typeof(data[0]));
-        console.log('\n');
-        console.log(JSON.parse(data[0]));
         setResponse(JSON.parse(data[0]));
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={prompt}
-                onChange={(e) => setPrompt(e.target.value)}
-                placeholder="Enter prompt"
-            />
-            <button type="submit">Send</button>
-        </form>
+        <div className="w-full flex justify-center items-center p-4">
+            <form 
+                onSubmit={handleSubmit} 
+                className="w-[85%] flex items center gap-2 p-4p bg-white">
+                <input
+                    className="flex-grow border border-gray-300 rounded-lg px-4 py-3 focus:outline-none focus:ring-2 focus-ring-blue-500"
+                    type="text"
+                    value={prompt} 
+                    onChange={(e) => setPrompt(e.target.value)}
+                    placeholder="Enter prompt"
+                />
+                <button 
+                    type="submit" 
+                    className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
+                    >Send</button>
+            </form>
+        </div>
+        
     );
 }
