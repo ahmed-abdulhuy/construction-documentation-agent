2e9f4f15a66248efba51c42ae028ed34ca913525
Ahmed Abdelhay <ahmelhuy@gmail.com>
2025-11-18 13:09:51 +0300

    Remove old ui setup

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,8 @@
 'use client';
-import { RequestForm } from './components/request-form/request-form';
 
 export default function Home() {
   return (
     <div >
-      <RequestForm /> 
     </div>   
   );
 }
